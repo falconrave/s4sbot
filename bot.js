72c3df1a09da1809fb57dc2e4706753bfe8b0f78
Falconrave <penguintronder@gmail.com>
2017-10-13 11:57:57 -0700

    Setup bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'your bot token here';
+const token = 'MzY4NDcwMjM2MzA0NzY5MDI0.DMKcOQ.443gBxhab9tzZwZCKSu4INAXuyE';
+
+client.on('ready', () => {
+  console.log("I am ready");
+});
+
+client.on('message', message => {
+  if (message.conten === '!ai') {
+    // generate sentence
+  }
+});
+
+client.login(token);

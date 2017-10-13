5d984c7e5f9f69f8c3817994cd95b3356e10b1cb
Falconrave <penguintronder@gmail.com>
2017-10-13 12:15:12 -0700

    Add modules

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
+const request = require('request');
 const client = new Discord.Client();
 const token = '';
 
+var url = 'https://a.4cdn.org/s4s/1.json';
+
+request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body["threads"]);
+    }
+})
+
 client.on('ready', () => {
-  console.log("I am ready");
+    console.log("I am ready");
 });
 
 client.on('message', message => {
-  if (message.conten === '!ai') {
-    // generate sentence
-  }
+    if (message.content === '!ai') {
+        // generate sentence
+    }
 });
 
 client.login(token);
